94c7f459a803c1c9418dd49a9be5b75d61a47ba8
Sandy Hider <sandy.hider@jhuapl.edu>
2025-04-10 10:24:13 -0400

    latest changes

--- terraform/improvmx.tf
+++ terraform/improvmx.tf
@@ -1,11 +1,24 @@
-data "improvmx_domain_check" "hiderland_com" {
+# data "improvmx_domain_check" "hiderland_com" {
+#   domain = "hiderland.com"
+# }
+
+resource "improvmx_domain" "hiderland" {
   domain = "hiderland.com"
 }
+
+# import {
+#   to = improvmx_domain.hiderland
+#   id = "hiderland.com"
+# }
 
 resource "improvmx_email_forward" "sandy" {
   domain            = "hiderland.com"
   alias_name        = "sandy"
   destination_email = "hiderrt1@yahoo.com"
+  depends_on = [improvmx_domain.hiderland]
 }
 
-
+# import {
+#   to = improvmx_email_forward.sandy
+#   id = "hiderland.com_sandy"
+# }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,30 @@
 terraform {
-  cloud {
-    organization = "hiderland"
 
-    workspaces {
-      name = "hiderland"
-    }
-  }
+#   cloud {
+#     organization = "hiderland"
+
+#     workspaces {
+#       name = "hiderland"
+#     }
+#   }
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.89.0"
     }
+    improvmx = {
+      source  = "issyl0/improvmx"
+    }
   }
+
 }
 
 provider "aws" {
   region = "us-east-1"
+  profile = "mcbain68-terraform"
+}
+
+provider "improvmx" {
+  token = "sk_a90c617fa91c47e9b8e0709d5ace95c6"
 }
